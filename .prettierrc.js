dcdbc0400c8169ea2fcd53a291e5d606795e6463
fede-rodes <federodes@gmail.com>
2021-10-04 13:31:27 +0200

    adding lint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	arrowParens: 'avoid',
+	singleQuote: true,
+	tabWidth: 2,
+	printWidth: 90,
+	plugins: ['prettier-plugin-svelte'],
+	semi: false,
+	svelteSortOrder: 'options-scripts-markup-styles',
+	svelteStrictMode: false,
+	svelteBracketNewLine: true,
+	svelteIndentScriptAndStyle: true,
+	trailingComma: 'all',
+}
